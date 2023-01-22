feecfea3bd8391ad941272387acc3dc725285c2a
http <xinjingjie.us@gmail.com>
2023-01-22 15:31:50 -0500

    submit 1.1.0

--- app.js
+++ app.js
@@ -1,35 +1,17 @@
 'use strict';
-const pacProxy = require('pacproxy-js');
-var currentConfig = false;
 
-function setConfig(config){
-    currentConfig = config;
+// if website is empty, you can add page request handler or websocket handler
+
+exports.onrequest = false;
+exports.onconnection = false;
+
+
+//page request handler
+function onrequest(req, res){
+    console.log('all website url handled here except paclink' )
 }
 
-function httpsWorker(glx) {
-    pacProxy.load(currentConfig);
-
-    var httpsServer = glx.httpsServer(null, function(req, res) {
-        pacProxy.handleRequest(req, res);
-    });
-
-	httpsServer.on('connect', pacProxy.handleConnect);
-
-    httpsServer.listen(currentConfig.port, "0.0.0.0", function() {
-        console.info("Listening on ", httpsServer.address());
-    });
-
-    // Note:
-    // You must ALSO listen on port 80 for ACME HTTP-01 Challenges
-    // (the ACME and http->https middleware are loaded by glx.httpServer)
-    var httpServer = glx.httpServer();
-
-    httpServer.listen(currentConfig.httpport, "0.0.0.0", function() {
-        console.info("Listening on ", httpServer.address());
-        console.log("\r\nshare your pac url:  \r\n%s\r\n", 'https://'+ currentConfig.domain + currentConfig.paclink +"\r\n");
-    });
-
+//websocket handler
+function onconnection(ws, req){
+    console.log('all websocket url handled here except wsslink' )
 }
-
-exports.httpsWorker = httpsWorker;
-exports.setConfig = setConfig;
--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 #!/usr/bin/env node
+
 'use strict';
 
 const greenlock = require('greenlock-express');
 const fs = require('fs');
+const pacProxy = require('pacproxy-js');
 const app = require('./app.js');
-const { get } = require('http');
 
 var currentConfig = false;
 var accountEmail = false;
 
-exports.runServer = runServer;
 exports.startServer = startServer;
-
 runServer();
 
-function runServer(currentConfig){
+function runServer(vConfig){
 	if(!process.argv[1].includes(__filename)) return;  //used as a module
-    if(!currentConfig) {
+    if(!vConfig) {
         if(loadConfig()) startServer();
+    } else {
+        currentConfig = vConfig
+        startServer();
     }
 }
 
@@ -40,12 +42,6 @@ function loadConfig()
     console.log("maintainer: " + accountEmail + '\r\n');
     if(!accountEmail) return false;
 
-    if(!currentConfig.https) currentConfig.https = true;
-    if(!currentConfig.httpport) currentConfig.httpport = 80;
-    if(!currentConfig.port) currentConfig.port = 443;
-    if(!currentConfig.proxyport) currentConfig.proxyport = 443;
-
-    app.setConfig(currentConfig);
     return true;
 }
 
@@ -58,5 +54,41 @@ function startServer()
             maintainerEmail: accountEmail,
             cluster: false
         })
-        .ready(app.httpsWorker);
+        .ready(httpsWorker);
 }
+
+
+function httpsWorker(glx) {
+
+    if(!currentConfig.https) currentConfig.https = true;
+    if(!currentConfig.httpport) currentConfig.httpport = 80;
+    if(!currentConfig.port) currentConfig.port = 443;
+    if(!currentConfig.proxyport) currentConfig.proxyport = 443;
+    if(!currentConfig.innerport) currentConfig.innerport = 7513;
+    currentConfig.onrequest = app.onrequest;
+    currentConfig.onconnection = app.onconnection;
+
+    var httpsServer = glx.httpsServer(null, function(req, res) {
+        pacProxy.handleRequest(req, res);
+    });
+    currentConfig.server = httpsServer;
+    currentConfig.skiprequest = true;
+    pacProxy.proxy(currentConfig);
+
+    httpsServer.listen(currentConfig.port, "0.0.0.0", function() {
+        console.info("/r/n Https Listening on ", httpsServer.address());
+        console.info("/r/n Share your pacproxy link:  ", pacProxy.getShareLink('http'));
+        console.info("/r/n Share your wssproxy link:  ", pacProxy.getShareLink('ws'));
+    });
+
+    // Note:
+    // You must ALSO listen on port 80 for ACME HTTP-01 Challenges
+    // (the ACME and http->https middleware are loaded by glx.httpServer)
+    var httpServer = glx.httpServer();
+
+    httpServer.listen(currentConfig.httpport, "0.0.0.0", function() {
+        console.info("/r/n Http Listening on ", httpServer.address());
+    });
+
+}
+
